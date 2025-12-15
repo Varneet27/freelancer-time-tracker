ada57d632317ae8a1a7ed2c0355ef0e56c79ba30
Kunal <km6048879@gmail.com>
2025-12-15 22:50:05 +0530

    time tracker app

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectDB = require('./config/db');
+connectDB();
+
+const authRoutes = require('./routes/auth.routes');
+const clientRoutes = require('./routes/client.routes');
+const projectRoutes = require('./routes/project.routes');
+const timeRoutes = require('./routes/time.routes');
+const invoiceRoutes = require('./routes/invoice.routes');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/clients', clientRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/time', timeRoutes);
+app.use('/api/invoices', invoiceRoutes);
+
+module.exports = app;
+
+
